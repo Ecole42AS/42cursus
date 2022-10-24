a2323de6fa97a29392afa01d9a5657d928d45e9d
Alexandre Stutz <alexandre.stutz@hotmail.com>
2022-10-24 21:44:46 +0200

    myFirstBranch first commit

---
+++ Libft/ft_isalpha.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+#include "stdio.h"
+
+int		ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int main()
+{
+  int res = ft_isalpha(65);
+  printf("%d", res);
+}
