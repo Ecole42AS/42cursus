f04e25bf0c378d03e1d0c45f13279a3c8d862693
Alexandre Stutz <alexandre.stutz@hotmail.com>
2022-10-29 10:57:13 +0200

    29.10M

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,13 +6,28 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 14:11:54 by astutz            #+#    #+#             */
-/*   Updated: 2022/10/28 14:11:57 by astutz           ###   ########.fr       */
+/*   Updated: 2022/10/29 10:54:57 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stdio.h"
 
-void *memset(void *s, int c, size_t n)
+void *memset(void *str, int c, size_t len)
 {
-	
+	unsigned char *ptr;
+
+	ptr = str;
+	while (len > 0)
+	{
+		*ptr = c;
+		ptr++;
+		len--;
+	}
+}
+
+int main()
+{
+	printf("%p", memset("h", 1, 2));
+	return (0);
 }
