0c2eeabb8967320d8e381f5dbf729bb5018b2b21
astutz <alexandre.stutz@hotmail.com>
2022-11-22 17:48:20 +0100

    gacp

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 08:49:30 by astutz            #+#    #+#             */
-/*   Updated: 2022/11/20 10:49:19 by astutz           ###   ########.fr       */
+/*   Updated: 2022/11/22 17:47:33 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,53 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*lstnew;
-	t_list	*lstfinal;
+	t_list	*newlst;
+	t_list	*newobj;
+	void	*newcontent;
 
-	if (!lst || !f)
+	if (!f || !del)
 		return (NULL);
-	lstfinal = NULL;
+	newlst = NULL;
 	while (lst)
 	{
-		lstnew = ft_lstnew(f(lst->content));
-		if (!lstnew)
+		newcontent = f(lst->content);
+		newobj = ft_lstnew(newcontent);
+		if (!newobj)
 		{
-			ft_lstclear(&lst, del);
+			free(newcontent);
+			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&lstfinal, lstnew);
+		ft_lstadd_back(&newlst, newobj);
 		lst = lst->next;
 	}
-	return (lstfinal);
+	return (newlst);
 }
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list	*lstnew;
+// 	t_list	*lstfinal;
+// 	void	*tmp;
+
+// 	if (!f || !del)
+// 		return (NULL);
+// 	lstfinal = NULL;
+// 	while (lst)
+// 	{
+// 		tmp = f(lst->content);
+// 		lstnew = ft_lstnew(tmp);
+// 		if (!lstnew)
+// 		{
+// 			free(tmp);
+// 			ft_lstclear(&lst, del);
+// 			return (NULL);
+// 		}
+// 		ft_lstadd_back(&lstfinal, lstnew);
+// 		lst = lst->next;
+// 	}
+// 	return (lstfinal);
+// }
 
 // t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 // {
