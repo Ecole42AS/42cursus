0ee5cfc53c1b0a2155a2eb49338c6e83df0e49d0
Alexandre Stutz <alexandre.stutz@hotmail.com>
2022-11-23 20:28:03 +0100

    gacp

--- ft_printf/libft/ft_split.c
+++ ft_printf/libft/ft_split.c
@@ -6,220 +6,166 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:01:57 by astutz            #+#    #+#             */
-/*   Updated: 2022/11/20 10:58:15 by astutz           ###   ########.fr       */
+/*   Updated: 2022/11/22 17:27:49 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** Description
-** Alloue (avec malloc(3)) et
-** retourne un tableau de chaines de caractères “fraiches”
-** (toutes terminées par un ’\0’, le tableau également donc)
-** résultant de la découpe de s selon le caractère c.
-** Si l’allocation echoue, la fonction retourne NULL.
-** Exemple : ft_strsplit("*salut*les***etudiants*", ’*’)
-** renvoie le tableau ["salut", "les", "etudiants"].
-** Param. #1
-** La chaine de caractères à découper.
-** Param. #2
-** Le caractère selon lequel découper la chaine.
-** Retour
-** Le tableau de chaines de caractères “fraiches” résultant de la découpe.
-*/
+// /*
+// ** Description
+// ** Alloue (avec malloc(3)) et
+// ** retourne un tableau de chaines de caractères “fraiches”
+// ** (toutes terminées par un ’\0’, le tableau également donc)
+// ** résultant de la découpe de s selon le caractère c.
+// ** Si l’allocation echoue, la fonction retourne NULL.
+// ** Exemple : ft_strsplit("*salut*les***etudiants*", ’*’)
+// ** renvoie le tableau ["salut", "les", "etudiants"].
+// ** Param. #1
+// ** La chaine de caractères à découper.
+// ** Param. #2
+// ** Le caractère selon lequel découper la chaine.
+// ** Retour
+// ** Le tableau de chaines de caractères “fraiches” résultant de la découpe.
+// */
 
 #include "libft.h"
 
-static unsigned int	ft_is_c(const char c, const char t)
-{
-	return (c == t);
-}
+// static unsigned int	ft_is_c(const char c, const char t)
+// {
+// 	return (c == t);
+// }
 
-static size_t	ft_count(char const *s, char const c)
-{
-	size_t	count;
-	size_t	i;
+// static size_t	ft_count(char const *s, char const c)
+// {
+// 	size_t	count;
+// 	size_t	i;
 
-	if (s[0] != 0)
-	{
-		i = 1;
-		if (ft_is_c(s[0], c) == 0)
-			count = 1;
-		else
-			count = 0;
-		while (s[i])
-		{
-			if (ft_is_c(s[i - 1], c) == 1
-				&& ft_is_c(s[i], c) == 0)
-				count++;
-			i++;
-		}
-		return (count);
-	}
-	else
-		return (0);
-}
+// 	if (s[0] != 0)
+// 	{
+// 		i = 1;
+// 		if (ft_is_c(s[0], c) == 0)
+// 			count = 1;
+// 		else
+// 			count = 0;
+// 		while (s[i])
+// 		{
+// 			if (ft_is_c(s[i - 1], c) == 1
+// 				&& ft_is_c(s[i], c) == 0)
+// 				count++;
+// 			i++;
+// 		}
+// 		return (count);
+// 	}
+// 	else
+// 		return (0);
+// }
 
-static size_t	ft_wordlen(const char *str, const char c)
-{
-	size_t	i;
+// static size_t	ft_wordlen(const char *str, const char c)
+// {
+// 	size_t	i;
 
-	i = 0;
-	while (str[i] && !ft_is_c(str[i], c))
-		i++;
-	return (i);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**sp;
-	size_t	i;
-	size_t	j;
-	size_t	k;
-
-	sp = (char **)malloc(sizeof(char *) * (ft_count(s, c) + 1));
-	if (!s || !sp)
-		return (NULL);
-	i = 0;
-	j = 0;
-	k = 0;
-	while ((k = 0) || s[i])
-	{
-		while (s[i] && ft_is_c(s[i], c))
-			i++;
-		if (s[i])
-		{	
-			sp[j] = (char *)malloc(sizeof(char) * (ft_wordlen(s + i, c) + 1));
-			if (!sp[j])
-				return (NULL);
-			while (s[i] && !ft_is_c(s[i], c))
-				sp[j][k++] = s[i++];
-			sp[j++][k] = '\0';
-		}
-	}
-	sp[j] = '\0';
-	return (sp);
-}
+// 	i = 0;
+// 	while (str[i] && !ft_is_c(str[i], c))
+// 		i++;
+// 	return (i);
+// }
 
 // char	**ft_split(char const *s, char c)
 // {
 // 	char	**sp;
-// 	int		nbwords;
-// 	int		i;
-// 	int		cursor;
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	k;
 
-// 	nbwords = 0;
-// 	i = 0;
-// 	cursor = 0;
-// 	nbwords = count_words(s, c);
-// 	sp = ft_calloc(nbwords + 1, sizeof(char *));
-// 	if (sp == NULL)
+// 	sp = (char **)malloc(sizeof(char *) * (ft_count(s, c) + 1));
+// 	if (!s || !sp)
 // 		return (NULL);
-// 	while (i < nbwords)
+// 	i = 0;
+// 	j = 0;
+// 	k = 0;
+// 	while ((k = 0) || s[i])
 // 	{
-// 		sp[i] = wordcpy(s, &cursor, c);
-// 		i++;
+// 		while (s[i] && ft_is_c(s[i], c))
+// 			i++;
+// 		if (s[i])
+// 		{	
+// 			sp[j] = (char *)malloc(sizeof(char) * (ft_wordlen(s + i, c) + 1));
+// 			if (!sp[j])
+// 				return (NULL);
+// 			while (s[i] && !ft_is_c(s[i], c))
+// 				sp[j][k++] = s[i++];
+// 			sp[j++][k] = '\0';
+// 		}
 // 	}
-// 	sp[i] = NULL;
+// 	sp[j] = '\0';
 // 	return (sp);
 // }
 
-// char	*wordcpy(const char *src, int *cursor, char c)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*dest;
+static int	ft_count(char const *s, char c)
+{
+	int	i;
+	int	word;
 
-// 	i = 0;
-// 	j = 0;
-// 	src += *cursor;
-// 	dest = ft_calloc(wordlen(src, c) + 1, sizeof(char));
-// 	if (dest == NULL)
-// 		return (NULL);
-// 	while (*src == c)
-// 	{
-// 		src++;
-// 		i++;
-// 	}
-// 	while (*src != c && *src)
-// 	{
-// 		dest[j] = *src++;
-// 		i++;
-// 		j++;
-// 	}
-// 	dest[j] = 0;
-// 	*cursor += i;
-// 	return (dest);
-// }
+	i = 0;
+	word = 0;
+	while (s && s[i])
+	{
+		if (s[i] != c)
+		{
+			word++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (word);
+}
 
-// static int	count_words(char const *s, char c)
-// {
-// 	int	words;
-// 	int	i;
+static int	ft_size(char const *s, char c, int i)
+{
+	int	t;
 
-// 	words = 0;
-// 	i = 0;
-// 	if (!s)
-// 		return (0);
-// 	if (s[i] != c)
-// 	{
-// 		i++;
-// 		words++;
-// 	}
-// 	while (s[i] != '\0')
-// 	{
-// 		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-// 			words++;
-// 		i++;
-// 	}
-// 	return (words);
-// }
+	t = 0;
+	while (s[i] != c && s[i])
+	{
+		t++;
+		i++;
+	}
+	return (t);
+}
 
-// static char	*make_string(char const *s, char c)
-// {
-// 	char	*word;
-// 	int		i;
+static char	**ft_free(char **final, int j)
+{
+	while (j-- > 0)
+		free(final[j]);
+	free(final);
+	return (NULL);
+}
 
-// 	i = 0;
-// 	while (s[i] && s[i] != c)
-// 		i++;
-// 	word = (char *)malloc (sizeof(char) * (i + 1));
-// 	if (!word)
-// 		return (NULL);
-// 	i = 0;
-// 	while (s[i] && s[i] != c)
-// 	{
-// 		word[i] = s[i];
-// 		i++;
-// 	}
-// 	word[i] = '\0';
-// 	return (word);
-// }
+char	**ft_split(char const *s, char c)
+{
+	int		j;
+	int		word;
+	char	**final;
+	int		size;
+	int		i;
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**result;
-// 	int		num_words;
-// 	int		i;
-
-// 	if (!s)
-// 		return (NULL);
-// 	i = 0;
-// 	num_words = count_words(s, c);
-// 	result = (char **)malloc (sizeof(char *) * (num_words + 1));
-// 	if (!result)
-// 		return (NULL);
-// 	while (*s)
-// 	{
-// 		while (*s && *s == c)
-// 			s++;
-// 		if (*s && *s != c)
-// 		{
-// 			result[i] = make_string (s, c);
-// 			i++;
-// 			while (*s && *s != c)
-// 				s++;
-// 		}
-// 	}
-// 	result[i] = NULL;
-// 	return (result);
-// }
+	i = 0;
+	word = ft_count(s, c);
+	final = (char **)malloc(sizeof(char *) * (word + 1));
+	if (!final)
+		return (NULL);
+	j = -1;
+	while (++j < word)
+	{
+		while (s[i] == c)
+			i++;
+		size = ft_size(s, c, i);
+		final[j] = ft_substr(s, i, size);
+		if (!final[j])
+			return (ft_free(final, j));
+		i += size;
+	}
+	final[j] = 0;
+	return (final);
+}
--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 19:25:59 by astutz            #+#    #+#             */
-/*   Updated: 2022/11/22 19:37:07 by astutz           ###   ########.fr       */
+/*   Updated: 2022/11/23 20:07:48 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static void formating(char c, va_list ap)
+static void	formating(char c, va_list ap)
 {
 	if (c == 'c')
 		ft_putchar_fd(va_arg(ap, int), 1);
@@ -36,33 +36,31 @@ static void formating(char c, va_list ap)
 
 int	ft_printf(const char *format, ...)
 {
-	int	param_nbr;
-	int	i;
+	int		param_nbr;
+	int		i;
 	va_list	ap;
-	
+
 	va_start(ap, format);
-	// param_nbr = get_param_nbr(format);
 	i = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			formating(format[++i], ap);
-			
 		}
 		else
 			write(1, &format[i], 1);
 		i++;
 	}
 	va_end(ap);
-
-    return (1);
+	return (1);
 }
 
-// int main()
+// int	main(int ac, char *av)
 // {
-// 	//printf("test c: %c \n test s: 1) %s\t2) %s\n.", 'Z', "test1", "test2");
-	
-// 	ft_printf("\n\ntest c: %c \ntest s: 1) %s\t2) %s \ntest p : %p\ntest d: %d \ntest i: 1) %i\t 2) %i\ntest u: 1)%u\t 2) %u\ntest x: %x\ntest X: %X\ntest pourcent: %%\n.", 'Z', "test1", "test2", "TESTP", 12345, 213, -9876543, 4294967292, 3294967292, 198, 198);
-	
+// 	ft_printf("\n\ntest c: %c \ntest s: 1) %s\t2) %s \ntest$
+	// p : %p\ntest d: %d \ntest i: 1) %i\t 2) %i\ntest u: 1)%u\t 2)
+	// %u\ntest x: %x\ntest X: %X\ntest pourcent: %%\n.", 'Z', "test1",
+	// "test2", "TESTP", 12345, 213, -9876543, 4294967292, 3294967292,
+	// 198, 198);
 // }
