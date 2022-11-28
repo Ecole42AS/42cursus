f23f73922f7a15daeca93facdd6f290268d2e301
astutz <alexandre.stutz@hotmail.com>
2022-11-28 16:34:56 +0100

    gacp

--- ft_printf/srcs/ft_putunbr.c
+++ ft_printf/srcs/ft_putunbr.c
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 10:13:29 by astutz            #+#    #+#             */
-/*   Updated: 2022/11/28 16:00:10 by astutz           ###   ########.fr       */
+/*   Updated: 2022/11/28 16:33:21 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,3 +40,48 @@
 // 	return (len);
 // }
 
+static int	sizemalloc(unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (n > 9)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i + 1);
+}
+
+static char	*uitoa(unsigned int n)
+{
+	char	*ptr;
+	int		i;
+
+	i = sizemalloc(n);
+	ptr = malloc(sizeof(char) * (i + 1));
+	if (ptr == 0)
+		return (NULL);
+	ptr[i] = '\0';
+	if (n == 0)
+		ptr[0] = 48;
+	while (n != '\0')
+	{
+		ptr[i - 1] = n % 10 + 48;
+		n = n / 10;
+		i--;
+	}
+	return (ptr);
+}
+
+int	ft_putunbr(unsigned int nb)
+{
+	char	*str;
+	int		len;
+
+	str = uitoa(nb);
+	ft_putstr_fd(str, 1);
+	len = ft_strlen(str);
+	free (str);
+	return (len);
+}
