e574ac25bfc27a08b637e578374fbf2472b8571d
Alexandre Stutz <alexandre.stutz@hotmail.com>
2022-12-19 21:20:24 +0100

    gacp

--- test/print_hex.c
+++ test/print_hex.c
@@ -6,16 +6,38 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 19:33:31 by astutz            #+#    #+#             */
-/*   Updated: 2022/12/19 21:17:02 by astutz           ###   ########.fr       */
+/*   Updated: 2022/12/19 21:20:12 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-int main()
+int	ft_atoi(char *str)
 {
-	char str[] = "hello";
+	int n = 0;
 
-	printf("%c", *str);
+	while (*str != '\0')
+	{
+		n = n * 10;
+		n = n + *str - '0';
+		++str;
+	}
+	return (n);
+}
+
+void	print_hex(int n)
+{
+	char hex_digits[] = "0123456789abcdef";
+
+	if (n >= 16)
+		print_hex(n / 16);
+	write(1, &hex_digits[n % 16], 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		print_hex(ft_atoi(argv[1]));
+	write(1, "\n", 1);
 }
