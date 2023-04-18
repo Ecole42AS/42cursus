bf076e01b82ddff63939f5bfcf717898236bffc2
Alexandre Stutz <alexandre.stutz@hotmail.com>
2023-04-18 20:16:30 +0200

    gacp

---
+++ minitalk/client.c
@@ -0,0 +1,46 @@
+#include "ft_printf/ft_printf.h"
+
+void	send_bit(int pid, char c)
+{
+	int	shift;
+
+	shift = 0;
+	while (shift < 8)
+	{
+		if ((c >> shift) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		shift++;
+		usleep(100);
+	}
+}
+
+void	ft_send_str(int pid, char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		send_bit(pid, str[i]);
+		i++;
+	}
+	send_bit(pid, '\0');
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*str;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		ft_send_str(pid, str);
+	}
+	else
+		ft_printf("\nYOU EITHER LEFT IT BLANK OR ARE DOING MORE THAN 1 WORD\n\n");
+	return (0);
+}
