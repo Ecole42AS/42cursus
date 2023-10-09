ec65c355fb79706b07e25c42b6de5cb2ad3f8c73
Alexandre Stutz <alexandre.stutz@hotmail.com>
2023-10-09 22:24:27 +0200

    gacp

---
+++ exam04/microshell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 20:55:27 by astutz            #+#    #+#             */
+/*   Updated: 2023/10/09 21:49:21 by astutz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
