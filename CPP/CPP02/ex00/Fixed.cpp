f14b21044f6f10f44cf6b3bc123e07b19b3d0707
alex <alexandre.stutz@hotmail.com>
2024-05-27 21:30:12 +0200

    starting cpp02 ex00

--- CPP/CPP02/ex00/Fixed.cpp
+++ CPP/CPP02/ex00/Fixed.cpp
@@ -6,16 +6,44 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 11:07:42 by astutz            #+#    #+#             */
-/*   Updated: 2024/05/27 11:09:47 by astutz           ###   ########.fr       */
+/*   Updated: 2024/05/27 21:29:06 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed() {}
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
 
 Fixed::~Fixed() {}
 
-Fixed::Fixed(Fixed const &src) {}
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
 
-Fixed::Fixed(Fixed const &rhs) {}
+Fixed &Fixed::operator=(Fixed const &rhs)
+{
+	if (this != &rhs)
+	{
+		std::cout << "Copy assignment operator called" << std::endl;
+		this->_decimalBits = rhs.getRawBits();
+
+		return *this;
+	}
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_decimalBits);
+}
+
+void Fixed::setRawBits(int const newDecimalBits)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_decimalBits = newDecimalBits;
+
+}
--- CPP/CPP02/ex00/Fixed.hpp
+++ CPP/CPP02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 10:31:24 by astutz            #+#    #+#             */
-/*   Updated: 2024/05/27 11:07:34 by astutz           ###   ########.fr       */
+/*   Updated: 2024/05/27 21:25:33 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@
 class Fixed
 {
 private:
-	int realNumber;
-	static const int decimal; //nb de bit utiliser pour représenter les chiffres après la virgule
+	int _realNumber;
+	static int const _decimalBits = 8; //nb de bit utiliser pour représenter les chiffres après la virgule
 public:
 	Fixed();
 	~Fixed();
 	Fixed(Fixed const &src); //constructeur par copie 
-	Fixed &operator=(Fixed const &rhs); //rhs = right-hand side, opérateur d'assignation par copie
+	Fixed &operator=(Fixed const &rhs); //rhs = right-hand side, opérateur d'assignation par copie, est utilisée pour copier les valeurs d'un objet à un autre déjà existant de la même classe.(utile pour des ressources dynamiques)
+	int getRawBits() const;
+	void setRawBits(int const newDecimalBits);
+
 };
