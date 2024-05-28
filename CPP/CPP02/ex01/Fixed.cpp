8539410c43c6518a1948146bdcc3c25fa41313e0
alex <alexandre.stutz@hotmail.com>
2024-05-28 11:50:51 +0200

    ex01 cpp02 in progress

--- CPP/CPP02/ex01/Fixed.cpp
+++ CPP/CPP02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:12:22 by astutz            #+#    #+#             */
-/*   Updated: 2024/05/28 10:50:49 by astutz           ###   ########.fr       */
+/*   Updated: 2024/05/28 11:40:41 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,43 @@ Fixed::Fixed(const Fixed &src)
 	*this = src;
 }
 
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		this->_decimalBits = rhs.getRawBits();
+	return *this;
+}
 
+Fixed::Fixed(const int i)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_decimalBits = i << _fractionalBits;
+}
+
+Fixed::Fixed(const float f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_decimalBits = roundf(f * (1 << _fractionalBits));
+}
+
+
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_decimalBits;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_decimalBits = raw;
+}
+
+	float toFloat() const;
+	int toInt() const;
 
 std::ostream &operator<<(std::ostream &os, const Fixed &rhs) {
-	os << rhs.toFloat(); // Affiche la valeur brute (_decimalBits) de l'objet Fixed
+	os << rhs.toFloat(); // Affiche la valeur convertie (_decimalBits) de l'objet rhs
 	return os;
 }
--- CPP/CPP02/ex01/Fixed.hpp
+++ CPP/CPP02/ex01/Fixed.hpp
@@ -6,13 +6,14 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:12:50 by astutz            #+#    #+#             */
-/*   Updated: 2024/05/28 10:50:22 by astutz           ###   ########.fr       */
+/*   Updated: 2024/05/28 11:40:22 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <string>
+#include <math.h>
 
 class Fixed
 {
@@ -26,6 +27,8 @@ public:
 	Fixed &operator=(const Fixed &rhs);
 	Fixed(const int i);
 	Fixed(const float f);
+	int getRawBits() const;
+	void setRawBits(int const raw);
 	float toFloat() const;
 	int toInt() const;
 };
