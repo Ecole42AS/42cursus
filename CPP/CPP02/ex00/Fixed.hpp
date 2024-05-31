7db67db9643e5b603b23f98021aa5ecc70763910
astutz <alexandre.stutz@hotmail.com>
2024-05-31 14:17:53 +0200

    ex00 cpp02 make again jekyll pb

--- CPP/CPP02/ex00/Fixed.hpp
+++ CPP/CPP02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 10:31:24 by astutz            #+#    #+#             */
-/*   Updated: 2024/05/29 10:44:38 by astutz           ###   ########.fr       */
+/*   Updated: 2024/05/31 14:14:31 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 class Fixed
 {
 private:
-	int _nb; //valeur brute du nombre en virgule fixe. Cette valeur est stockée sous forme d'un entier et est utilisée pour représenter à la fois la partie entière et la partie fractionnaire du nombre.
+	int _decimalBits; //valeur brute du nombre en virgule fixe. Cette valeur est stockée sous forme d'un entier et est utilisée pour représenter à la fois la partie entière et la partie fractionnaire du nombre.
 	static int const _fractionalBits = 8; //nb de bit utiliser pour représenter les chiffres après la virgule
 public:
 	Fixed();
@@ -26,9 +26,5 @@ public:
 	Fixed &operator=(Fixed const &rhs); //rhs = right-hand side, opérateur d'assignation par copie, est utilisée pour copier les valeurs d'un objet à un autre déjà existant de la même classe.(utile pour des ressources dynamiques)
 	int getRawBits() const;
 	void setRawBits(int const raw);
-};
 
-std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
-	os << fixed._nb; // Affiche la valeur brute (_nb) de l'objet Fixed
-	return os;
-}
+};
