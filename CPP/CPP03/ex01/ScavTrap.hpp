8a01bd6f77f00c0d645a348209ef5f3d051cd8c3
astutz <alexandre.stutz@hotmail.com>
2024-05-31 11:47:12 +0200

    cpp03 ex00 done

---
+++ CPP/CPP03/ex01/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 10:42:10 by astutz            #+#    #+#             */
+/*   Updated: 2024/05/31 10:44:48 by astutz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+private:
+	/* data */
+public:
+	ScavTrap(/* args */);
+	~ScavTrap();
+};
+
+ScavTrap::ScavTrap(/* args */)
+{
+}
+
+ScavTrap::~ScavTrap()
+{
+}
+
