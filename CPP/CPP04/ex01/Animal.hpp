27abcfee387301b95941fe63cffc85ec13ef38b3
alex <alexandre.stutz@hotmail.com>
2024-06-04 11:10:11 +0200

    try to make abstract class ex02 cpp04

--- CPP/CPP04/ex01/Animal.hpp
+++ CPP/CPP04/ex01/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 09:46:45 by astutz            #+#    #+#             */
-/*   Updated: 2024/06/04 10:24:16 by astutz           ###   ########.fr       */
+/*   Updated: 2024/06/04 11:10:01 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ public:
 	virtual ~Animal();
 	Animal(const Animal &src);
 	Animal &operator=(const Animal &rhs);
-	virtual void makeSound() const = 0;
+	virtual void makeSound() const;
 	std::string getType() const;
 };
--- CPP/CPP04/ex02/Animal.cpp
+++ CPP/CPP04/ex02/Animal.cpp
@@ -6,47 +6,84 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 09:50:39 by astutz            #+#    #+#             */
-/*   Updated: 2024/06/04 10:35:12 by astutz           ###   ########.fr       */
+/*   Updated: 2024/06/04 11:09:20 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include "Animal.hpp"
+
+// Animal::Animal() : _type("Animal")
+// {
+// 	std::cout << "Animal default constructor called" << std::endl;
+// }
+
+// Animal::Animal(std::string type) : _type(type)
+// {
+// 	std::cout << "Type animal constructor called" << std::endl;
+// }
+
+// Animal::~Animal()
+// {
+// 	std::cout << "Animal destructor called" << std::endl;
+// }
+
+// Animal::Animal(const Animal &src)
+// {
+// 	std::cout << "Animal copy constructor called" << std::endl;
+// 	*this = src;
+// }
+
+// Animal &Animal::operator=(const Animal &rhs)
+// {
+//     std::cout << "Animal copy assignment operator called" << std::endl;
+// 	if (this != &rhs)
+// 		_type = rhs._type;
+// 	return *this;
+// }
+
+// // void Animal::makeSound() const
+// // {
+// // 	std::cout << "No sound's emitted.." << std::endl;
+// // }
+
+// std::string Animal::getType() const
+// {
+// 	return _type;
+// }
+
+// Animal.cpp
 #include "Animal.hpp"
 
 Animal::Animal() : _type("Animal")
 {
-	std::cout << "Animal default constructor called" << std::endl;
+    std::cout << "Animal default constructor called" << std::endl;
 }
 
 Animal::Animal(std::string type) : _type(type)
 {
-	std::cout << "Type animal constructor called" << std::endl;
+    std::cout << "Type animal constructor called" << std::endl;
 }
 
 Animal::~Animal()
 {
-	std::cout << "Animal destructor called" << std::endl;
+    std::cout << "Animal destructor called" << std::endl;
 }
 
 Animal::Animal(const Animal &src)
 {
-	std::cout << "Animal copy constructor called" << std::endl;
-	*this = src;
+    std::cout << "Animal copy constructor called" << std::endl;
+    *this = src;
 }
 
 Animal &Animal::operator=(const Animal &rhs)
 {
     std::cout << "Animal copy assignment operator called" << std::endl;
-	if (this != &rhs)
-		_type = rhs._type;
-	return *this;
+    if (this != &rhs)
+        _type = rhs._type;
+    return *this;
 }
-
-// void Animal::makeSound() const
-// {
-// 	std::cout << "No sound's emitted.." << std::endl;
-// }
 
 std::string Animal::getType() const
 {
-	return _type;
+    return _type;
 }
