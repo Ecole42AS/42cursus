bb651e5f5b81669978868cdb8ce94dca7b4f7854
alex <alexandre.stutz@hotmail.com>
2024-06-05 16:34:52 +0200

    finally i finished

--- CPP/CPP04/ex03/main.cpp
+++ CPP/CPP04/ex03/main.cpp
@@ -6,69 +6,31 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:30:28 by astutz            #+#    #+#             */
-/*   Updated: 2024/06/05 16:03:13 by astutz           ###   ########.fr       */
+/*   Updated: 2024/06/05 16:34:43 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include "AMateria.hpp"  
-#include "Character.hpp"  
-#include "Cure.hpp"  
-#include "ICharacter.hpp"  
-#include "IMateriaSource.hpp"  
-#include "Ice.hpp"  
-#include "MateriaSource.hpp"
-// #include "Ice.hpp""
-// #include "Cure.hpp"
-// #include "Character.hpp"
-// #include "MateriaSource.hpp"
-#include "AMateria.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
 #include "Character.hpp"
 #include "MateriaSource.hpp"
-int	main(void)
+
+int main()
 {
-	IMateriaSource* 	src = new MateriaSource();
+	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-
-	ICharacter* 		me = new Character("me");
-
+	ICharacter* me = new Character("me");
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-
-	ICharacter* 		bob = new Character("bob");
-
+	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-
 	delete bob;
 	delete me;
 	delete src;
-
-	return (0);
+	return 0;
 }
-
-// int main()
-// {
-// 	IMateriaSource* src = new MateriaSource();
-// 	src->learnMateria(new Ice());
-// 	src->learnMateria(new Cure());
-// 	ICharacter* me = new Character("me");
-// 	AMateria* tmp;
-// 	tmp = src->createMateria("ice");
-// 	me->equip(tmp);
-// 	tmp = src->createMateria("cure");
-// 	me->equip(tmp);
-// 	ICharacter* bob = new Character("bob");
-// 	me->use(0, *bob);
-// 	me->use(1, *bob);
-// 	delete bob;
-// 	delete me;
-// 	delete src;
-// 	return 0;
-// }
