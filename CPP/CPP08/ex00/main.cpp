1058635868e14429123448b729a22a500bc13a87
alex <alexandre.stutz@hotmail.com>
2024-06-29 13:59:50 +0200

    finish ex00 CPP08

--- CPP/CPP08/ex00/main.cpp
+++ CPP/CPP08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 12:06:56 by astutz            #+#    #+#             */
-/*   Updated: 2024/06/29 13:55:26 by astutz           ###   ########.fr       */
+/*   Updated: 2024/06/29 13:59:09 by astutz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,24 @@ int main() {
 	vec.push_back(3);
 	vec.push_back(5);
 
-    // Tester la fonction printVec
     std::cout << "Contenu du vecteur : ";
     printVec(vec);
 
-    // Tester la fonction easyFind avec une valeur présente dans le vecteur
-    try {
+    try 
+	{
         easyFind(vec, 3);
-    } catch (const std::runtime_error& e) {
+    }
+	catch (const std::runtime_error& e)
+	{
         std::cerr << "Erreur : " << e.what() << std::endl;
     }
 
-    // Tester la fonction easyFind avec une valeur absente du vecteur
-    try {
+    try 
+	{
         easyFind(vec, 10);
-    } catch (const std::runtime_error& e) {
+    }
+	catch (const std::runtime_error& e)
+	{
         std::cerr << "Erreur : " << e.what() << std::endl;
     }
 
