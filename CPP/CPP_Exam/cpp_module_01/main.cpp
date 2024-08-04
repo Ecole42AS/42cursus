37c2af5b57066cec6d7b77aa55dd8b7e5850e8ad
alex <alexandre.stutz@hotmail.com>
2024-08-04 09:23:10 +0200

    module01 starting

---
+++ CPP/CPP_Exam/cpp_module_01/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astutz <astutz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 17:19:50 by astutz            #+#    #+#             */
+/*   Updated: 2024/08/03 17:20:24 by astutz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Warlock.hpp"
+
+int main()
+{
+  Warlock const richard("Richard", "Mistress of Magma");
+  richard.introduce();
+  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+
+  Warlock* jack = new Warlock("Jack", "the Long");
+  jack->introduce();
+  jack->setTitle("the Mighty");
+  jack->introduce();
+
+  delete jack;
+
+  return (0);
+}
