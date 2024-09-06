e06bfe6accd3f6e82e9cae38532ed1281fb588cb
alex <alexandre.stutz@hotmail.com>
2024-09-06 23:08:25 +0200

    start parsing WebServ

---
+++ WebServ/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+
+int main() {
+    // for (int i = 0; i <= 100; ++i) {
+    //     std::cout << "\rProgress: " << i << "%" << std::flush;//return carriage
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    // }
+    // std::cout << std::endl << "Done!" << std::endl;
+    // return 0;
+	std::cout << "stackoverflow" << "\rnine " << std::endl;
+	return 0;
+
+}
+
+
+
