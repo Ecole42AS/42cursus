ddb997a048d4e44e4e4b39dea47b1c2010eee159
alex <alexandre.stutz@hotmail.com>
2024-11-23 19:04:05 +0100

    ajout d'un modèle  lié en one to one à user pour avoir un display_name

--- ft_transcendence/user/models.py
+++ ft_transcendence/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def user_directory_path(instance, filename): # instance est une instance du modèle Profile
+    # Le fichier sera uploadé vers MEDIA_ROOT/user_<id>/<filename>
+    return f'user_{instance.user.id}/{filename}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Un profil est lié à un et un seul utilisateur, si un utilisateur est supprimé, le profil est supprimé
+    display_name = models.CharField(max_length=50, unique=True)
+    avatar = models.ImageField(upload_to=user_directory_path, default='default_avatar.png')
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.display_name # Affiche le nom d'affichage du profil dans l'interface admin sous forme de chaîne de caractères
