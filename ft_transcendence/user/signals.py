b97785ac574cf8c37d2e672cda9e99a8cb68f9fe
alex <alexandre.stutz@hotmail.com>
2024-11-23 20:24:46 +0100

    add signals file to catch user creation and make the profile automatically

---
+++ ft_transcendence/user/signals.py
@@ -0,0 +1,15 @@
+# Les signaux dans Django permettent d'exécuter du code en réponse à des 
+# événements spécifiques, comme la sauvegarde ou la suppression d'un objet.
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver # Permet de recevoir des signaux
+from .models import Profile
+
+@receiver(post_save, sender=User) # Lorsqu'un objet User est sauvegardé, le signal post_save est envoyé et reçu par la fonction create_user_profile
+def create_user_profile(sender, instance, created, **kwargs): #created est un booléen qui indique si un objet a été créé (True) ou modifié (False)
+    if created:
+        Profile.objects.create(user=instance, display_name=instance.username) # Crée un profil lié à l'utilisateur
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
