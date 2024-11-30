dd57dd618c9eea424bf568a44cf9eac8da546241
alex <alexandre.stutz@hotmail.com>
2024-11-30 18:52:08 +0100

    suppression de l ancienne image avatar  lorsqu'on fait une update(PUT)

--- ft_transcendence/user/signals.py
+++ ft_transcendence/user/signals.py
@@ -5,6 +5,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver # Permet de recevoir des signaux
 from .models import Profile
 from django.conf import settings
+from django.db import transaction
+import os
+from django.db.models.signals import pre_save
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -17,3 +20,20 @@ def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
     else:
         Profile.objects.create(user=instance, display_name=instance.username)
+
+@receiver(pre_save, sender=Profile)
+def delete_old_avatar_on_update(sender, instance, **kwargs):
+    if not instance.pk: # Si l'instance n'a pas de clé primaire, c'est une création et non une mise à jour
+        return
+
+    try:
+        old_avatar = Profile.objects.get(pk=instance.pk).avatar
+    except Profile.DoesNotExist:
+        return
+
+    if old_avatar and old_avatar != instance.avatar:
+        def delete_old_file():
+            if os.path.isfile(old_avatar.path):  # Vérifie si le fichier existe avant de le supprimer
+                os.remove(old_avatar.path)
+
+        transaction.on_commit(delete_old_file)
