6b736f1b8af6ee0baaaa3fdaf4e5f95157c8ce56
alex <alexandre.stutz@hotmail.com>
2025-01-22 22:06:39 +0100

    all tests done in Makefile

--- inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,10 +1,11 @@
+# Vérification de la version PHP
 php-fpm7.3 -v >/dev/null 2>&1
 
 # Boucle verification mariadb
 while ! mariadb -u $MARIADB_USER --password=$MARIADB_PASS -h mariadb -P 3306 --silent 2>/dev/null;
 do
 	sleep 8
-	echo "Mariadb n'est pas encore pret"
+	echo "Mariadb n'est pas encore prêt"
 done
 echo "MariaDB est prêt"
 
@@ -26,6 +27,12 @@ else
 	# Configuration de WP : connexion à la base de données et création des utilisateurs
 	wp config create --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASS --dbhost=$WP_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root --quiet
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --quiet
+
+	# Sécurisation du fichier wp-config.php
+	echo "Sécurisation de wp-config.php..."
+	chmod 600 /var/www/wordpress/wp-config.php
+	chown www-data:www-data /var/www/wordpress/wp-config.php
+	echo "wp-config.php sécurisé."
 
 	# Création de l'administrateur
 	if wp user get "$WP_ADMIN_USER" --allow-root --path=/var/www/wordpress > /dev/null 2>&1; then
