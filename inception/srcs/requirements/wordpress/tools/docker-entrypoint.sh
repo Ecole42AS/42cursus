63ffe4956bea98d18171ebc059d30a46a1ac2f3f
alex <alexandre.stutz@hotmail.com>
2025-01-22 14:41:39 +0100

    add 2 users and modifiy conf file to redirect https and accept only astutz.42

--- inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -6,9 +6,9 @@ do
 	sleep 8
 	echo "Mariadb n'est pas encore pret"
 done
-	echo "MariaDB est prêt"
+echo "MariaDB est prêt"
 
-# Installation de Wordpress si necessaire
+# Installation de WordPress si nécessaire
 if [ -e /var/www/wordpress/wp-config.php ]
 then
 	echo "wp-config existe."
@@ -19,14 +19,29 @@ else
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
 
-	# Installation de wordpress
+	# Installation de WordPress
 	cd /var/www/wordpress
 	wp core download --allow-root --quiet
 
-	# Configuration de WP : connection a la base de donnees et creation des users
+	# Configuration de WP : connexion à la base de données et création des utilisateurs
 	wp config create --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASS --dbhost=$WP_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root --quiet
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --quiet
-	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root --quiet
+
+	# Création de l'administrateur
+	if wp user get "$WP_ADMIN_USER" --allow-root --path=/var/www/wordpress > /dev/null 2>&1; then
+		echo "L'utilisateur administrateur $WP_ADMIN_USER existe déjà."
+	else
+		wp user create "$WP_ADMIN_USER" "$WP_ADMIN_EMAIL" --role=administrator --user_pass="$WP_ADMIN_PASSWORD" --allow-root --path=/var/www/wordpress --quiet
+		echo "Utilisateur administrateur $WP_ADMIN_USER créé avec succès."
+	fi
+
+	# Création d'un utilisateur auteur distinct
+	if wp user get "$WP_AUTHOR_USER" --allow-root --path=/var/www/wordpress > /dev/null 2>&1; then
+		echo "L'utilisateur auteur $WP_AUTHOR_USER existe déjà."
+	else
+		wp user create "$WP_AUTHOR_USER" "$WP_AUTHOR_EMAIL" --role=author --user_pass="$WP_AUTHOR_PASSWORD" --allow-root --path=/var/www/wordpress --quiet
+		echo "Utilisateur auteur $WP_AUTHOR_USER créé avec succès."
+	fi
 
 	echo "WordPress installé avec succès."
 fi
